fa9c12483b4f8175bbda2f48a50fa8d224354b32
Student <student@RU-019.local>
2015-09-18 12:19:01 -0600

    shweet

--- proto/main.js
+++ proto/main.js
@@ -2,6 +2,8 @@ $(document).on('ready', function() {
   
 });
 
-var birthmonth = function(select) {
-	alert(select.options[select.selectedIndex].text)
-}
+// need to add an ng-hide on an ng-click for lightbox.
+// 	if age is over 21 will hide. 
+// need to create an ng-hide for all selects after each is entered.
+// 		after where you want to get beer pops up then have the types of beer pop up.
+	
