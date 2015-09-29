a79c44d9add0de6ae9403462f4c0507286e6a498
Student <student@RU-019.local>
2015-09-29 15:10:17 -0600

    one factory

--- proto/beer-types.js
+++ proto/beer-types.js
@@ -5,6 +5,7 @@ angular.module('beerApp', [])
 angular.module('beerApp')
 	.factory('beerFactory', function(){
 		var beerList = []
+		var breweryList = []
 			var Beer = function(name, flavors, strength) {
 			this.name = name
 			this.flavors = flavors
@@ -26,9 +27,59 @@ angular.module('beerApp')
 			var stout = new Beer('Stout', ['Heavy', 'Malty/Rich'], 'Strong')
 			var wheat = new Beer('Wheat', ['Crisp', 'Fruity'], 'Light')
 
+		var Brewery = function(name, location, address, website, specialties, beersss){
+			// name = string
+			// location = string
+			// website = string
+			// specialties = array
+			// beer = array of beers
+			this.name = name
+			this.location = location
+			this.address = address
+			this.website = website
+			this.specialties = specialties
+			this.beersss = beersss
+
+		
+
+			breweryList.push(this)
+		}
+	
+
+
+
+		var avery = new Brewery('Avery Brewing Company', 'Boulder', '4910 Nautilus Ct.', 'http://averybrewing.com', ['strongly flavored Ales', 'lots of hops, malt, AND FLAVOR', 'high ABV'], [ ipa, wheat, brown, stout, pilsner, porter, amber])
+		var leftHand = new Brewery('Left Hand Brewery', 'Longmont', '1265 Boston Ave.', 'http://lefthandbrewing.com', ['Awesome labels'], [ipa, stout, paleAle, pilsner, porter])
+		var boulderBeer = new Brewery('Boulder Beer', 'Boulder', '2880 Wilderness Pl.', 'http://boulderbeer.com', ["Colorado's first craft brewery", 'Barrel Aged Beers', 'Big Flavors'])
+		var fate = new Brewery('FATE Brewing Company', 'Boulder', '1600 38th St.', 'http://fatebrewingcompany.com', ['Creating something different'], [ipa, stout, wheat, pilsner])
+		var twistedPine = new Brewery('Twisted Pine Brewing', 'Boulder', '3201 Walnut St.', 'http://twistedpinebrewing.com', ['High quality products', 'experimentation'], [ipa, paleAle, saison, wheat, stout])
+		var southernSun = new Brewery('Southern Sun Pub and Brewery', 'Boulder', '1535 Pearl St.', 'http://mountainsunpub.com', ['Hoppy Beers', 'Ales'], [wheat, stout, paleAle, porter, amber, ipa])
+		var upslope = new Brewery('Uplsope Brewing Company', 'Boulder', '1898 S Flatiron Ct.', 'http://upslopebrewing.com', ['cool cans', 'Beers made in small batches'], [paleAle, ipa, brown, sour, stout])
+		var asher = new Brewery('Asher Brewing Comapny', 'Boulder', '4699 Nautilus Ct.', 'http://asherbrewing.com', ["Colorado's First all Organic Brewery"], [ipa, amber, wheat])
+		var sanitas = new Brewery('Sanitas Brewing Company', 'Boulder', '3550 Frontier Ave.', 'http://sanitasbreweing.com', ['Quirky beers', 'Lots of fruit, hops, and coffee'], [ipa, sour, brown, amber, stout, fruit])
+		var wildWoods = new Brewery('Wild Woods Brewery', 'Boulder', '5460 Conestoga Ct.', 'http://wildwoodsbrewery.com', ['Beers based on outdoor activities'], [ipa, paleAle, amber, porter, stout, wheat, fruit])
+		var westFlanders = new Brewery('West Flanders Brewing Company', 'Boulder', '1125 Pearl St.', 'http://wfbrews.com', ['balanced flavors', 'crazy stories'], [ipa, paleAle, pilsner, porter, sour])
+		var finkelGarf = new Brewery(' Finkel and Garf', 'Boulder', '5455 Spine Rd.', 'http://finkelandgarf.com', ['flavorful beers', 'Still kids at heart'], [stout, ipa, amber, wheat, fruit, sour] )
+		var vindication = new Brewery('Vindication Brewing Company', 'Boulder', '6880 Winchester Cir.', 'http://vindictionbrewing.com', ['Attention to Deatail', 'specialty equipment', 'Mixtures of flavors'], [ipa, amber, fruit, wheat, stout])
+		var bootstrap = new Brewery('Bootstrap Brewing Comapny', 'Niwot', '6778 N 79th St.', 'http://bootstrapbrewing.com', ['Easy to drink Beers with lots of flavor'], [amber, paleAle, wheat, ipa, brown, stout] )
+		var powderKeg = new Brewery('Powder Keg Brewing Co', 'Niwot', '101 2nd Ave.', 'http://powderkepbrewingcompany.com', ['American and Belgian-style Ales'], [stout, paleAle, saison, amber])
+		var frontRange = new Brewery('Front Range Brewing Company', 'Lafayette', '400 W South Boulder Rd. #1650', 'http://frontrangebrewingcompany.com', ['Americanizing foreign beer types'], [wheat, pilsner, amber, ipa, stout])
+		var thePost = new Brewery('The Post Brewing Comapny', 'Lafayette', '105 W Emma St.', 'http://postbrewing.com', ['strong flavors', 'drinkable beers'], [pilsner, paleAle, ipa])
+		var twelveDegree = new Brewery('12Degree Brewing', 'Louisville', '820 Main St', 'http://12degree.com', ['Belgian-inspired Artisinal Ales'], [saison, wheat, ipa, amber])
+		var fourNoses = new Brewery('4 Noses brewing Comapny', 'Broomfield', '8855 W 116th Cir. #4', 'http://4nosesbrewing.com', ['Barrel Aged Beers', 'Fresh Flavors'], [blonde, ipa, porter, paleAle])
+		var odd13 = new Brewery('Odd13 Brewing', 'Lafayette', '301 E Simpson St.', 'http://odd13brewing.com', ['beers with a story(no really)', 'reduced gluten beers'], [ipa, sour, amber, stout])
+		var gravity = new Brewery('Gravity Brewing Company', 'Louisville', '1150 Pine St.', 'http://thegravitybrewing.com', ['"One pint is worth a thousand words"'], [pilsner, fruit, saison, stout, wheat, ipa, amber])
+		var wonderland = new Brewery('Wonderland Brewing Company', 'Broomfield', '5450 W 120th Ave.', 'http://wonderlandbrewing.com', ['"Care, Creativity and Time"', 'beer as therapy'], [blonde, ipa, saison, fruit, porter, amber, stout, paleAle])
+		var crystalSprings = new Brewery('Crystal Springs Brewery', 'Louisvile', '657 S Taylor Ave.', 'http://crystalsprinsbrewing.com', ['small batches', 'seasonal beers'], [ipa, wheat, paleAle, fruit, porter, amber, stout])
+		var oskarBlues = new Brewery('Oskar Blues', 'Longmont', '1555 Hover Rd.', 'http://www.oskarblues.com', ['Hoppy beers', 'Growing off what they have previously done'], [ipa, pilsner, amber, stout])
+
+	
+	
 		return {
 			Beer : Beer,
-			beerList : beerList
+			beerList : beerList,
+			Brewery : Brewery,
+			breweryList : breweryList
 		}
 
 
@@ -38,23 +89,23 @@ angular.module('beerApp')
 })
 
 // pictures for card flip
-angular.module('beerApp')
-	.factory('picFactory', function(){
-	var picArray = []
-		var Picture = function(picture) { 
-		this.picture = picture
+// angular.module('beerApp')
+// 	.factory('picFactory', function(){
+// 	var picArray = []
+// 		var Picture = function(picture) { 
+// 		this.picture = picture
 
-		picArray.push(this)
+// 		picArray.push(this)
 
-	}
-			// var picAmber = new Picture('http://learn.kegerator.com/wp-content/uploads/2013/11/red_amber_ale.jpg') Will work but need to have saved images not links. 
+// 	}
+// 			// var picAmber = new Picture('http://learn.kegerator.com/wp-content/uploads/2013/11/red_amber_ale.jpg') Will work but need to have saved images not links. 
 
-		return {
-			Picture : Picture,
-			picArray : picArray
-		}	
+// 		return {
+// 			Picture : Picture,
+// 			picArray : picArray
+// 		}	
 
-	})
+// 	})
 
 angular.module('beerApp')
 	.factory('cityFactory', function(){
@@ -89,7 +140,7 @@ angular.module('beerApp')
 
 $scope.beers = beerFactory.beerList
 
-$scope.pictures = ['beer-test.jpg', 'beer-test2.jpg']
+// $scope.pictures = ['beer-test.jpg', 'beer-test2.jpg']
 
 // Accordion test	
 
